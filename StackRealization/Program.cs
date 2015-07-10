4ca73e4cac326f08c54dd64c9d505ffe9a607173
Artem <cinquestyle@gmail.com>
2015-07-10 12:34:12 +0300

    Implemented a class and core functionality

---
+++ StackRealization/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StackRealization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+            stack.Push(2);
+            stack.Push(44);
+            stack.Push(3);
+            stack.Push(12);
+            Console.WriteLine(stack);
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Count);
+        }
+    }
+}
---
+++ StackRealization/Stack.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+
+namespace StackRealization
+{
+    public class Stack
+    {
+        private const int DefaultSize = 5;
+
+        private int[] _array;
+        private int _top;
+
+        public int Count
+        {
+            get { return _top + 1; }
+        }
+
+        public Stack()
+        {
+            _top = -1;
+            _array = new int[DefaultSize];
+        }
+
+        public void Push(int item)
+        {
+            if (_top == _array.Length + 1)
+                Array.Resize(ref _array, _array.Length*2);
+            _array[++_top] = item;
+        }
+
+        public int Pop()
+        {
+            if (_top == -1)
+                throw new InvalidOperationException();
+            return _array[_top--];
+        }
+
+        public int Peek()
+        {
+            if (_top == -1)
+                throw new InvalidOperationException();
+            return _array[_top];
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stackString = new StringBuilder();
+            for (int i = 0; i < _top + 1; i++)
+                stackString.Append(_array[i] + " ");
+            return stackString.ToString();
+        }
+    }
+}
